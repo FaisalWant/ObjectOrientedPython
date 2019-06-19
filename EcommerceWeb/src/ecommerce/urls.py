2d52346157727822e378e781b420b1883af88299
xander <heliconrngr@gmail.com>
2019-06-19 19:41:10 +0530

    ecommerce

--- EcommerceWeb/src/ecommerce/urls.py
+++ EcommerceWeb/src/ecommerce/urls.py
@@ -18,15 +18,21 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import home_page, about_page, contact_page, login_page , register_page 
+from products.views import ProductListView, product_list_view, ProductDetailView, product_detail_view
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^home/', home_page, name='home'),
     url(r'^login/', login_page, name='login_page'),
     url(r'^register/', register_page, name='register _page'),
-
+    url(r'^products/$', ProductListView.as_view(), name='register _page'),
+    url(r'^products-fbv/$',product_list_view),
     url(r'^$', home_page),
     url(r'^about/', about_page, name='about'),
     url(r'^contact/', contact_page, name='contact'),
+    url(r'^products/(?P<pk>\d+)/$', ProductDetailView.as_view(), name='register _page'),
+    url(r'^products-fbv/(?P<pk>\d+)/$',product_detail_view),
 
 
 ]
--- EcommerceWeb/src/products/models.py
+++ EcommerceWeb/src/products/models.py
@@ -1,10 +1,28 @@
 from django.db import models
+import random 
+import os
 
+def get_filename_ext(filpath):
+	base_name=os.path.basename(filepath)
+	name, ext=os.path.splitext(base_name)
+	return name, ext
+
+def upload_image_path(instance, filename):
+	print(instance)
+	print(filename)
+	new_filename=random.randint(1,39)
+	name, ext= get_filename_ext(filename)
+	final_filename= '{new_filename}{ext}'.format(new_filename=new_filename, ext=ext)
+	return "products/{new_filename}/{final_filename}".format(
+		new_filename=new_filename,
+		final_filename=final_filename
+		)
 # Create your models here.
 class Product(models.Model):  
 	title = models.CharField(max_length=123)
 	description=models.TextField()
 	price= models.DecimalField(decimal_places=2, max_digits=10, default=39.9)
-
-
- 
+	image= models.ImageField(upload_to='products/',null= True, blank=True)
+	def __str__(self):
+		return self.title
+		
